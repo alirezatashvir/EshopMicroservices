c25db927be0870c537bc4ecb9c09e4631ca3a0d5
Alireza <alireza.tashvir@gmail.com>
2025-12-20 21:10:26 +0100

    Add intial Catalog API and Building Blocks projects

---
+++ eshop-microservices/src/BuildingBlocks/BuildingBlocks/CQR/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQR
+{
+    public interface ICommand : IRequest<Unit>
+    {
+    }
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
---
+++ eshop-microservices/src/BuildingBlocks/BuildingBlocks/CQR/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BuildingBlocks.CQR
+{
+    internal interface IQuery<out TResponse> : IRequest<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
