65e42ec29d520ec9fb31c4d9c0089d350ae93113
Alireza <alireza.tashvir@gmail.com>
2026-01-18 16:51:41 +0100

    Add IRepository pattern for Basket API using Marten library for BasketRepository

--- eshop-microservices/src/Services/Basket/Basket.API/GlobalUsing.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -4,3 +4,6 @@ global using Carter;
 global using Mapster;
 global using MediatR;
 global using FluentValidation;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Exceptions;
+global using Marten;
