3361528963b3220070e81dde3a0982e4745e0e03
Alireza <alireza.tashvir@gmail.com>
2026-01-18 13:38:17 +0100

    Add Carter package and a refrence to building blocks

--- eshop-microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
+++ eshop-microservices/src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoints.cs
@@ -13,7 +13,13 @@ namespace Basket.API.Basket.GetBasket
                 var result = sender.Send(new GetBasketQuery(userName));
                 var response = result.Adapt<GetBasketResponse>();
 
+                return Results.Ok(response);
             })
+            .WithName("GetBasket")
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Basket")
+            .WithDescription("Get Basket");
         }
     }
 }
